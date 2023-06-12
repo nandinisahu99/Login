148cebbc94abffe0c2e5161d49281eea4a5afd48
nandinisahu99 <115337833+nandinisahu99@users.noreply.github.com>
2023-06-12 18:47:49 +0530

    Update user_controller.js

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -2,8 +2,8 @@ const db=require("../model/db.js");
 const bcrypt=require("bcryptjs");
 const registerUser= async(req, res)=>{
     try{
-        const {name, email, roll, password, securityQuestion}=req.body;
-        if(!email || !password || !name || !roll || !securityQuestion){ 
+        const {name, email, roll, password, securityQuestion, securityAnswer}=req.body;
+        if(!email || !password || !name || !roll || !securityQuestion || !securityAnswer){ 
             return res.json({status: "error", error: "Please Enter your all details"});}
         else{
             db.query('select email from candidates where email=?',[email],async(err,result)=>{
@@ -11,7 +11,8 @@ const registerUser= async(req, res)=>{
                 if(result[0]) return res.json({status: "error", error: "Email has already been registered"})
                 else{
                     const Hpassword =await bcrypt.hash(password,8);
-                    db.query('insert into candidates set ?',{email:email,name:name,password:Hpassword,roll:roll,security:securityQuestion},(error,results)=>{
+                    const security= securityQuestion.trim()+"+"+securityAnswer.trim();
+                    db.query('insert into candidates set ?',{email:email,name:name,password:Hpassword,roll:roll,security:security},(error,results)=>{
                         if(error) throw error;
                         return res.json({status:"success", success:"User has been registered"})
                     })
